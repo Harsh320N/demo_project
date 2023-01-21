218a1a865f2447c3c133bb78bbfe53320d27201b
HARSH <harshnavapara320@gmail.com>
2023-01-21 23:37:28 +0530

    button added

---
+++ lib/common/widgets/buttons.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:user_listing_with_signup_demo/common/widgets/decoration.dart';
+import 'package:user_listing_with_signup_demo/common/widgets/text_styles.dart';
+import 'package:user_listing_with_signup_demo/utils/utils_export.dart';
+
+Widget button({
+  required String text,
+  double buttonHeight = 45.0,
+  double buttonWidth = 350.0,
+  Color fontColor = white,
+  FontWeight fontWeight = FontWeight.normal,
+  double fontSize = 14.0,
+}) =>
+    Container(
+      height: buttonHeight,
+      width: buttonWidth,
+      decoration: circularBoxDecoration(
+        circularRadius: 15.0,
+        gradient: const LinearGradient(
+          colors: [
+            primaryColor,
+            secondaryColor,
+          ],
+        ),
+      ),
+      child: labels(
+        text: text,
+        color: fontColor,
+        fontWeight: fontWeight,
+        size: fontSize,
+      ),
+    );
