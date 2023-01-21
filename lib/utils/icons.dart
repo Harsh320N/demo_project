6632fa96e8dd30627b718109e73be70d248554c8
HARSH <harshnavapara320@gmail.com>
2023-01-21 23:32:55 +0530

    icon and color added

--- lib/utils/icons.dart
+++ lib/utils/icons.dart
@@ -5,3 +5,4 @@ const IconData icReportGmailErrorRounded = Icons.report_gmailerrorred_rounded;
 const IconData icChevronLeft = CupertinoIcons.chevron_left;
 const IconData icCheckmarkAltCircle = CupertinoIcons.checkmark_alt_circle;
 const IconData icPersonCropCircleFill = CupertinoIcons.person_crop_circle_fill;
+const IconData icEnvelopeFill = CupertinoIcons.envelope_fill;
